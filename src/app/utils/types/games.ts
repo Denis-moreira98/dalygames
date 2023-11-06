774ae7ec777e04dfc5241e149446e8cf509bc663
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-11-06 15:46:02 -0300

    feat: criando interface games

---
+++ src/app/utils/types/games.ts
@@ -0,0 +1,9 @@
+export interface GameProps {
+   id: number;
+   title: string;
+   description: string;
+   img_url: string;
+   platforms: string[];
+   category: string[];
+   releases: string;
+}
