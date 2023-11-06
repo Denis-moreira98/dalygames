d0e036e0b077e58de832621da3384f34fe7ab1bb
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-11-06 16:11:19 -0300

    feat: criando pag home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 import { Container } from "@/components/container";
+import { GameProps } from "@/utils/types/games";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Home() {
+async function getDalyGame() {
+   try {
+      const res = await fetch(
+         `${process.env.NEXT_API_URL}/next-api/?api=game_day`
+      );
+      return res.json();
+   } catch (err) {
+      throw new Error("failed to fetch data");
+   }
+}
+export default async function Home() {
+   const dalyGames: GameProps = await getDalyGame();
+
    return (
-      <main className="flex">
+      <main className="w-full">
          <Container>
             <h1 className="text-center font-bold text-xl mt-8 mb-5">
                Separamos um jogo exclusivo para você
--- src/utils/types/games.ts
+++ src/utils/types/games.ts
@@ -2,7 +2,7 @@ export interface GameProps {
    id: number;
    title: string;
    description: string;
-   img_url: string;
+   image_url: string;
    platforms: string[];
    category: string[];
    releases: string;
