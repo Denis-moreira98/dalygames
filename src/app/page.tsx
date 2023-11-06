dcb0a0f8f45a418efd6c918439102c923308955c
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-11-06 14:55:47 -0300

    feat: criando componente container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Container } from "@/components/container";
+
 export default function Home() {
    return (
       <main className="flex">
-         <h1>TESTE</h1>
+         <Container>
+            <h1 className="text-center font-bold text-xl mt-8 mb-5">
+               Separamos um jogo exclusivo para você
+            </h1>
+         </Container>
       </main>
    );
 }
