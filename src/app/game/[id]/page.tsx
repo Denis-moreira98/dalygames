73899da501dd910fdf0e0cc147c3119107629ea0
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-11-10 19:19:53 -0300

    feat: revalidate

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -8,8 +8,10 @@ import { GameCard } from "@/components/gameCard";
 async function getData(id: string) {
    try {
       const res = await fetch(
-         `${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`
+         `${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`,
+         { cache: "no-store" }
       );
+
       return res.json();
    } catch (err) {
       throw new Error("Failed to fetch data");
@@ -19,7 +21,8 @@ async function getData(id: string) {
 async function getGameSorted() {
    try {
       const res = await fetch(
-         `${process.env.NEXT_API_URL}/next-api/?api=game_day`
+         `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+         { cache: "no-store" }
       );
       return res.json();
    } catch (err) {
