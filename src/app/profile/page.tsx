b3e3d5ba70ea6eda1d8330c1b7730084ee4711d5
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-11-13 15:55:35 -0300

    feat: metadata da page profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,12 @@ import Image from "next/image";
 import userImg from "public/user.png";
 import { FaShareAlt } from "react-icons/fa";
 import { FavoriteCard } from "./components/favorite";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+   title: "Meu perfil - Daly Games sua plataforma de jogos",
+   description: "Perfil Denis Moreira",
+};
 
 export default function Profile() {
    return (
